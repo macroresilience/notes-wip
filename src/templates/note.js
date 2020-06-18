20e0d167199f1a0f7bf9071532b3191a76d63b0c
personal <macroresilience@gmail.com>
2020-06-18 14:18:36 +0100

    add created and modified time to post

--- src/templates/note.js
+++ src/templates/note.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -10,6 +11,7 @@ export default function Note({ data }) {
     	<SEO title={note.frontmatter.title} description={note.excerpt} />
       <div>
         <h1>{note.frontmatter.title}</h1>
+        <p css={css`color: #bbb;`}>created {note.parent.birthTime}, modified {note.parent.modifiedTime}.</p>
         <div dangerouslySetInnerHTML={{ __html: note.html }} />
       </div>
     </Layout>
@@ -24,6 +26,12 @@ export const query = graphql`
         title
       }
       excerpt
+      parent {
+      ... on File {
+        modifiedTime(fromNow: true)
+        birthTime(fromNow: true)
+      }
+    }
     }
   }
 `
