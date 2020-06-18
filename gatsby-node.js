ab8fa70cd2a0eb9fab23e39f601f7bd36819b031
personal <macroresilience@gmail.com>
2020-06-18 18:08:08 +0100

    basic tags implementation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require(`path`)
+const _ = require("lodash")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -13,30 +14,58 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
+  const noteTemplate = path.resolve("src/templates/note.js")
+  const tagTemplate = path.resolve("src/templates/tags.js")
   const result = await graphql(`
-    query {
-      allMarkdownRemark {
+    {
+      postsRemark: allMarkdownRemark {
         edges {
           node {
             fields {
               slug
             }
+            frontmatter {
+              tags
+            }
           }
+        }
+      }
+      tagsGroup: allMarkdownRemark(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
         }
       }
     }
   `)
-
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  // handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  const notes = result.data.postsRemark.edges
+  // Create note pages
+  notes.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/templates/note.js`),
+      component: noteTemplate,
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
         slug: node.fields.slug,
+      },
+    })
+  })
+  // Extract tag data from query
+  const tags = result.data.tagsGroup.group
+  // Make tag pages
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+      component: tagTemplate,
+      context: {
+        tag: tag.fieldValue,
       },
     })
   })
---
+++ src/templates/tags.js
@@ -0,0 +1,83 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// Components
+import { Link, graphql } from "gatsby"
+
+const Tags = ({ pageContext, data }) => {
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allFile
+  const tagHeader = `${totalCount} note${
+    totalCount === 1 ? "" : "s"
+  } tagged with "${tag}"`
+
+  return (
+    <div>
+      <h1>{tagHeader}</h1>
+      <ul>
+        {edges.map(({ node }) => {
+          const { slug } = node.childMarkdownRemark.fields
+          const { title } = node.childMarkdownRemark.frontmatter
+          return (
+            <li key={slug}>
+              <Link to={slug}>{title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+      {/*
+              This links to a page that does not yet exist.
+              You'll come back to it!
+            */}
+      <Link to="/tags">All tags</Link>
+    </div>
+  )
+}
+
+Tags.propTypes = {
+  pageContext: PropTypes.shape({
+    tag: PropTypes.string.isRequired,
+  }),
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      totalCount: PropTypes.number.isRequired,
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            frontmatter: PropTypes.shape({
+              title: PropTypes.string.isRequired,
+            }),
+            fields: PropTypes.shape({
+              slug: PropTypes.string.isRequired,
+            }),
+          }),
+        }).isRequired
+      ),
+    }),
+  }),
+}
+
+export default Tags
+
+export const pageQuery = graphql`
+  query($tag: String) {
+    allFile(sort: {fields: [birthTime], order: DESC}, filter: {childMarkdownRemark: {frontmatter: {tags: {in: [$tag]}}}}) {
+    totalCount
+    edges {
+      node {
+        id
+        modifiedTime(fromNow: true)
+        birthTime(fromNow: true)
+        childMarkdownRemark {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+}
+`
